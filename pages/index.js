4ac07efe857709319b5234baf6c6be8092fed56b
Aryan Pitliya <38190250+aryanxp@users.noreply.github.com>
2021-07-24 13:18:58 +0530

    Navbar done

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import "tailwindcss/tailwind.css";
 import Header from "../components/Header";
+import Nav from "../components/Nav";
 export default function Home() {
   return (
     <div>
@@ -9,6 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <Nav />
     </div>
   );
 }
---
+++ utils/requests.js
@@ -0,0 +1,33 @@
+/* eslint-disable import/no-anonymous-default-export */
+const API_KEY = process.env.API_KEY;
+
+export default {
+  fetchTrending: {
+    title: "Trending",
+    url: `/trending/all.week?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchTopRated: {
+    title: "TopRated",
+    url: `/trending/all.week?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchActionMovies: {
+    title: "Action",
+    url: `/trending/all.week?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchComedyMovies: {
+    title: "Comedy",
+    url: `/trending/all.week?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchHorrorMovies: {
+    title: "Horror",
+    url: `/trending/all.week?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchRomanceMovies: {
+    title: "Romance",
+    url: `/trending/all.week?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchMysteryMovies: {
+    title: "Mystery",
+    url: `/trending/all.week?api_key=${API_KEY}&language=en-US`,
+  },
+};
