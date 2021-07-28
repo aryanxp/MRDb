b5315915802f1d3a5f1cafe39d717641e29b170f
Aryan Pitliya <38190250+aryanxp@users.noreply.github.com>
2021-07-28 10:41:29 +0530

    finalize

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
-
-function Thumbnail({ result }) {
+import { forwardRef } from "react";
+// eslint-disable-next-line react/display-name
+const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
+
   return (
-    <div className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+    <div
+      ref={ref}
+      className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
+    >
       <Image
         layout="responsive"
         src={
@@ -29,6 +34,6 @@ function Thumbnail({ result }) {
       </div>
     </div>
   );
-}
+});
 
 export default Thumbnail;
