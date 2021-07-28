3e1e3ac88bb9e9be3fa88313097038294e21b7d6
Aryan Pitliya <38190250+aryanxp@users.noreply.github.com>
2021-07-28 10:10:16 +0530

    thumbnail done

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,10 @@ import Head from "next/head";
 import "tailwindcss/tailwind.css";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
-export default function Home() {
+import Results from "../components/Results";
+import requests from "../utils/requests";
+export default function Home({ results }) {
+  console.log(results);
   return (
     <div>
       <Head>
@@ -11,6 +14,23 @@ export default function Home() {
       </Head>
       <Header />
       <Nav />
+      <Results results={results} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
--- utils/requests.js
+++ utils/requests.js
@@ -4,30 +4,30 @@ const API_KEY = process.env.API_KEY;
 export default {
   fetchTrending: {
     title: "Trending",
-    url: `/trending/all.week?api_key=${API_KEY}&language=en-US`,
+    url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   },
   fetchTopRated: {
     title: "TopRated",
-    url: `/trending/all.week?api_key=${API_KEY}&language=en-US`,
+    url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   },
   fetchActionMovies: {
     title: "Action",
-    url: `/trending/all.week?api_key=${API_KEY}&language=en-US`,
+    url: `/trending/all/week?api_key=${API_KEY}&with_genres=28`,
   },
   fetchComedyMovies: {
     title: "Comedy",
-    url: `/trending/all.week?api_key=${API_KEY}&language=en-US`,
+    url: `/trending/all/week?api_key=${API_KEY}&with_genres=35`,
   },
   fetchHorrorMovies: {
     title: "Horror",
-    url: `/trending/all.week?api_key=${API_KEY}&language=en-US`,
+    url: `/trending/all.week?api_key=${API_KEY}&with_genres=27`,
   },
   fetchRomanceMovies: {
     title: "Romance",
-    url: `/trending/all.week?api_key=${API_KEY}&language=en-US`,
+    url: `/trending/all/week?api_key=${API_KEY}&with_genres=10749`,
   },
   fetchMysteryMovies: {
     title: "Mystery",
-    url: `/trending/all.week?api_key=${API_KEY}&language=en-US`,
+    url: `/trending/all/week?api_key=${API_KEY}&with_genres=9648`,
   },
 };
