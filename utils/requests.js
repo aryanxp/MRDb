6b86f02fc64568da6b757ba7afc5d468b01e6480
Aryan Pitliya <38190250+aryanxp@users.noreply.github.com>
2021-07-29 18:03:03 +0530

    links fixed

--- utils/requests.js
+++ utils/requests.js
@@ -8,26 +8,26 @@ export default {
   },
   fetchTopRated: {
     title: "TopRated",
-    url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   },
   fetchActionMovies: {
     title: "Action",
-    url: `/trending/all/week?api_key=${API_KEY}&with_genres=28`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   },
   fetchComedyMovies: {
     title: "Comedy",
-    url: `/trending/all/week?api_key=${API_KEY}&with_genres=35`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
   },
   fetchHorrorMovies: {
     title: "Horror",
-    url: `/trending/all.week?api_key=${API_KEY}&with_genres=27`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
   },
   fetchRomanceMovies: {
     title: "Romance",
-    url: `/trending/all/week?api_key=${API_KEY}&with_genres=10749`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
   },
   fetchMysteryMovies: {
     title: "Mystery",
-    url: `/trending/all/week?api_key=${API_KEY}&with_genres=9648`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
   },
 };
