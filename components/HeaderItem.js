2bece6f15accf2ee0ca30d9634d8a71d9490852d
Aryan Pitliya <38190250+aryanxp@users.noreply.github.com>
2021-12-28 23:35:26 +0530

    removed login function

--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -1,18 +1,10 @@
-import { motion } from "framer-motion";
-
-function HeaderItem({ Icon, title, onClick }) {
+function HeaderItem({ Icon, title }) {
   return (
-    <div>
-      <motion.button
-        className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white"
-        onClick={onClick}
-      >
-        <Icon className="h-8 mb-1 animate-animated group-hover:animate-bounce" />
-        <p className="opacity-0 group-hover:opacity-100 tracking-widest">
-          {title}
-        </p>
-      </motion.button>
-      
+    <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white">
+      <Icon className="h-8 mb-1 animate-animated group-hover:animate-bounce" />
+      <p className="opacity-0 group-hover:opacity-100 tracking-widest">
+        {title}
+      </p>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,9 @@ import Header from "../components/Header";
 import Nav from "../components/Nav";
 import Results from "../components/Results";
 import requests from "../utils/requests";
+import Modal from "../components/Modal";
 import { useState } from 'react';
-import Login from "../components/Login";
+import { AnimatePresence } from "framer-motion";
 export default function Home({ results }) {
   const [modalOpen, setModalOpen] = useState(false);
   const close = () => setModalOpen(false);
@@ -18,7 +19,14 @@ export default function Home({ results }) {
       <Header onClick={() => (modalOpen ? close() : open())} />
       <Nav />
       <Results results={results} />
-      <Login />
+      {modalOpen && <Modal modalOpen={modalOpen} handleClose={close} />}
+      <AnimatePresence
+        initial={false}
+        exitBeforeEnter={true}
+        onExitComplete={() => null}
+      >
+        {modalOpen && <Modal modalOpen={modalOpen} handleClose={close} />}
+      </AnimatePresence>
     </div>
   );
 }
